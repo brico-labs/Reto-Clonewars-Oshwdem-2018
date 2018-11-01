2ec2fd8b71bd647503ad1f6b5916ad61ecec7eb8
Shiul93 <lfllamas93@gmail.com>
2018-11-01 22:21:30 +0100

    Added comments to the gear calculator script

--- gearcalculator.js
+++ gearcalculator.js
@@ -1,29 +1,47 @@
-var main_gear_Z = 20;
+
+
+// Primitive diameter of the base gear
 var main_gear_Dp = 31*2; //milimeters
+// Max allowed modulus
 var max_m = 50;
-var delta_m = 0.01;
+// Minimum number of teeth
 var min_z = 1;
+// Maximum number of teeth
 var max_z = 50;
 
+// Tolerance in the number of teeth
 var tolerance = 0.1;
 
+// Primitive diameters of the other gears
 var other_diameters = [56.68*2,62*2,38.31*2]
+
+// Empty array for the solutions
 var solutions = [];
 
+// Iterate over the number of teeth
 for (z = min_z; z <= max_z; z++ ){
+    // Compute modulus for that number of teeth
     var m = main_gear_Dp /z;
+
     pass = true;
+
+    // Check if all the other gears are in the acceptable tolerance
     other_diameters.forEach((dp)=>{
+        // Compute number of teeth
         var z = dp / m;
         if (pass && ((z % 1)>tolerance)){
             pass = false;
         }
     })
+
+    // If all the gears passed the tolerance test, add to the solutions array
     if (pass){
         solutions.push(m);
     }
 
 }
+
+// Print the solutions
 solutions.forEach((modulus, index)=>{
     console.log("Solution "+index+", Modulus: "+modulus);
     console.log("Gear 0, Dp: "+ main_gear_Dp.toFixed(2) + " Z: "+ (main_gear_Dp / modulus).toFixed(2))
